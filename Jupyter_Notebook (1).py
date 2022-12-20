2e25ef330b9e196176ecccdfb7e8648d0e90b4b2
ayushkr1805 <121079019+ayushkr1805@users.noreply.github.com>
2022-12-20 17:24:02 +0530

    Add files via upload

---
+++ Jupyter_Notebook (1).py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <center>
+#     <img src="https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DS0105EN-SkillsNetwork/labs/Module2/images/SN_web_lightmode.png" width="300" alt="cognitiveclass.ai logo"  />
+# </center>
+# 
+
+# #### Add your code below following the instructions given in the course
+# 
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+# **AYUSH KUMAR**
+# **STUDENT**
+
+# *I am interested in data science to pursue my further career*
+
+# # my code is to print first n whole numbers
+
+# In[ ]:
+
+
+#print number from 0 to n
+n= int(input())
+for i in range(0,n+1,1):
+    print(i)
+
+
+# In[ ]:
+
+
+print("__"*25)
+
+
+# In[ ]:
+
+
+
+
